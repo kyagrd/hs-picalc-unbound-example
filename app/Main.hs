b273f2d711bcf11e4633824f4c80caa969c47768
Ki Yung Ahn <kyagrd@gmail.com>
2017-04-27 16:20:24 +0800

    one step rule for fintie pic implemented

--- app/Main.hs
+++ app/Main.hs
@@ -3,11 +3,11 @@
 {-# LANGUAGE MultiParamTypeClasses     #-}
 {-# LANGUAGE NoMonomorphismRestriction #-}
 {-# LANGUAGE ScopedTypeVariables       #-}
-{-# LANGUAGE TemplateHaskell           #-}
+-- {-# LANGUAGE TemplateHaskell           #-}
 {-# LANGUAGE UndecidableInstances      #-}
 module Main where
 
-import qualified Lib
+import           Lib
 
 import           Control.Applicative
 import           Control.Monad
@@ -17,50 +17,40 @@ import           Control.Monad.Trans.State.Strict
 -- import           Data.Hashable
 -- import qualified Data.HashMap.Strict              as Map
 import           Data.Maybe
-import           Unbound.LocallyNameless          hiding (empty, fresh, join)
-import qualified Unbound.LocallyNameless          as LN
+-- import           Unbound.LocallyNameless          hiding (empty, fresh, join)
+-- import qualified Unbound.LocallyNameless          as LN
+import           Unbound.LocallyNameless          hiding (empty)
 
 {-# ANN module "HLint: ignore Use fmap" #-}
 {-# ANN module "HLint: ignore Use mappend" #-}
 
-
-type NamePr = Name Pr
-
-data Pr
-  = Var NamePr
-  | Null
-  | TauP Pr
-  | In NamePr (Bind NamePr Pr)
-  | Out NamePr NamePr (Bind NamePr Pr)
-  | Nu (Bind NamePr Pr)
-  | Bang Pr
-  | Par Pr Pr
-  | Plus Pr Pr
-  | Match NamePr NamePr Pr
-  deriving Show
-
-instance Eq Pr where (==) = aeq
-
-$(derive [''Pr])
-
-instance Alpha Pr
-
-instance Subst Pr Pr where
-  isvar (Var v) = Just (SubstName v)
-  isvar _       = Nothing
-
----- if you are to define unification kind of thing with Hashmap
--- instance Hashable NamePr where
---   hashWithSalt s n = hashWithSalt s (name2String n, name2Integer n)
-
-x, y :: NamePr
+x, y, z :: NameTm
 x = s2n "x"
 y = s2n "y"
+z = s2n "z"
 
 p1 = TauP Null `Plus` TauP (TauP Null)
-q1 = TauP (Match x y (TauP Null))
+q1 = TauP (Match (Var x) (Var y) (TauP Null))
 
 
+p2 = In (Var x) (bind z $ Out (Var x) (Var z) Null) `Par` Out (Var x) (Var y) Null
 
 main :: IO ()
-main = print "hello" -- Lib.main
+main = do
+  putStrLn $ "one step from: " ++ show p1
+  mapM_ print (runFreshMT (one p1) :: [(Act,Pr)])
+  putStrLn "================================================================"
+  putStrLn $ "one step from: " ++ show q1
+  mapM_ print (runFreshMT (one q1) :: [(Act,Pr)])
+  putStrLn "================================================================"
+  putStrLn $ "one step from: " ++ show p2
+  mapM_ print (runFreshMT (one p2) :: [(Act,Pr)])
+  putStrLn "================================================================"
+{-
+*Main Lib> :t runFreshMT (one p1)
+runFreshMT (one p1) :: MonadPlus m => m (Act, Pr)
+*Main Lib> runFreshMT (one p1) :: [(Act,Pr)]
+[(Tau,Null),(Tau,TauP Null)]
+*Main Lib> runFreshMT (one q1) :: [(Act,Pr)]
+[(Tau,Match (Var x) (Var y) (TauP Null))]
+-}
