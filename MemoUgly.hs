e034b6ca1c1e54c589578c4929b52eb68e2adb87
Ki Yung Ahn <kyagrd@gmail.com>
2017-07-07 20:52:47 +0800

    add missing file

---
+++ MemoUgly.hs
@@ -0,0 +1,10 @@
+module MemoUgly where
+import Control.Concurrent.MVar
+import qualified Data.Map as M
+import System.IO.Unsafe(unsafePerformIO)
+import qualified Data.MemoUgly
+
+memo f = Data.MemoUgly.memo f
+memoIO f = Data.MemoUgly.memoIO f
+
+memoFix ff = memo (ff f)
